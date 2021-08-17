3e1a963da0b0d4e3592860c860588cebf42c615a
Matt Berzinskas <mberz2@pdx.edu>
2021-08-17 12:12:17 -0700

    Site structure, navigation, resume outline, social media icons

--- js/nav.js
+++ js/nav.js
@@ -1,15 +1,22 @@
 const navbar = document.getElementById("navbar");
 const navbarToggle = navbar.querySelector(".navbar-toggle");
+const navbarMenu = navbar.querySelector(".navbar-menu");
+const navbarLinks = navbar
+	.querySelector(".navbar-links")
+	.querySelectorAll(".navbar-link");
 
 function openMobileNavbar() {
+	//console.log("Adding OPENED from classlist");
 	navbar.classList.add("opened");
 }
 
 function closeMobileNavbar() {
+	//console.log("Removing OPENED from classlist");
 	navbar.classList.remove("opened");
 }
 
 navbarToggle.addEventListener("click", () => {
+	//console.log("Clicked toggle [navbarToggle]");
 	if (navbar.classList.contains("opened")) {
 		closeMobileNavbar();
 	} else {
@@ -17,11 +24,13 @@ navbarToggle.addEventListener("click", () => {
 	}
 });
 
-const navbarMenu = navbar.querySelector(".navbar-menu");
-const navbarLinksContainer = navbar.querySelector(".navbar-links");
-
-navbarLinksContainer.addEventListener("click", (clickEvent) => {
-	clickEvent.stopPropagation();
-});
-
-navbarMenu.addEventListener("click", closeMobileNavbar);
+navbarLinks.forEach(e => {
+	e.addEventListener('click', function(){
+			//console.log("Clicked a link [navbarLink]");
+			if (navbar.classList.contains("opened")) {
+				closeMobileNavbar();
+			} else {
+				openMobileNavbar();
+			}
+	})
+})
