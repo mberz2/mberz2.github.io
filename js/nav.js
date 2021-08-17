9b5390cebbbcef45c1e486644931db1743c0da05
Matt Berzinskas <mberz2@pdx.edu>
2021-08-17 16:27:39 -0700

    Major rework to design, scripting, layout, and styles.

--- js/nav.js
+++ js/nav.js
@@ -1,9 +1,9 @@
-const navbar = document.getElementById("navbar");
-const navbarToggle = navbar.querySelector(".navbar-toggle");
-const navbarMenu = navbar.querySelector(".navbar-menu");
-const navbarLinks = navbar
+let navbar = document.getElementById("navbar");
+let navbarMenu = navbar.querySelector(".navbar-menu");
+let navbarLinks = navbar
 	.querySelector(".navbar-links")
 	.querySelectorAll(".navbar-link");
+let homeLink = navbar.querySelector(".home-link");
 
 function openMobileNavbar() {
 	//console.log("Adding OPENED from classlist");
@@ -15,22 +15,63 @@ function closeMobileNavbar() {
 	navbar.classList.remove("opened");
 }
 
-navbarToggle.addEventListener("click", () => {
-	//console.log("Clicked toggle [navbarToggle]");
+[
+	navbar.querySelector(".icon-bar"),
+	navbar.querySelector(".navbar-toggle"),
+].forEach((item) => {
+	item.addEventListener("click", () => {
+		console.log("Clicked toggle [navbarToggle2]");
+		if (navbar.classList.contains("opened")) {
+			closeMobileNavbar();
+		} else {
+			openMobileNavbar();
+		}
+	});
+});
+
+homeLink.addEventListener("click", () => {
+	console.log("Clicked toggle [navbarToggle]");
 	if (navbar.classList.contains("opened")) {
 		closeMobileNavbar();
-	} else {
-		openMobileNavbar();
 	}
 });
 
-navbarLinks.forEach(e => {
-	e.addEventListener('click', function(){
-			//console.log("Clicked a link [navbarLink]");
-			if (navbar.classList.contains("opened")) {
-				closeMobileNavbar();
-			} else {
-				openMobileNavbar();
-			}
-	})
-})
+navbarLinks.forEach((e) => {
+	e.addEventListener("click", function () {
+		console.log("Clicked a link [navbarLink]");
+		if (navbar.classList.contains("opened")) {
+			closeMobileNavbar();
+		} else {
+			openMobileNavbar();
+		}
+	});
+});
+
+/* Closes the navbar if the window is resized while its opened to a
+larger size, preventing some errors */
+window.addEventListener(
+	"resize",
+	function () {
+		if (
+			window.matchMedia("(min-width: 700px)").matches &&
+			navbar.classList.contains("opened")
+		) {
+			closeMobileNavbar();
+		}
+	},
+	true
+);
+
+let test = document.getElementById("test");
+test.addEventListener("mouseover", function (e) {
+	//console.log("MOUSEOVER");
+});
+
+$("#test").hover(
+	function () {
+		console.log("MOUSEOVER"), $(".navbar-menu").addClass("hover");
+	},
+	function () {
+		console.log("MOUSEOVER2"), $(".navbar-menu").removeClass("hover");
+	}
+);
