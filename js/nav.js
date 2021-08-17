9fa41bd9da4c1c239d9aadc8910a3b7a2b820911
Matt Berzinskas <mberz2@pdx.edu>
2021-08-17 00:27:56 -0700

    Refactor

--- js/nav.js
+++ js/nav.js
@@ -1,8 +1,29 @@
-function myFunction() {
-	var x = document.getElementById("myTopnav");
-	if (x.className === "topnav") {
-		x.className += " responsive";
-	} else {
-		x.className = "topnav";
-	}
+const navbar = document.getElementById("navbar");
+const navbarToggle = navbar.querySelector(".navbar-toggle");
+
+function openMobileNavbar() {
+	navbar.classList.add("opened");
+	navbarToggle.setAttribute("aria-label", "Close navigation menu.");
 }
+
+function closeMobileNavbar() {
+	navbar.classList.remove("opened");
+	navbarToggle.setAttribute("aria-label", "Open navigation menu.");
+}
+
+navbarToggle.addEventListener("click", () => {
+	if (navbar.classList.contains("opened")) {
+		closeMobileNavbar();
+	} else {
+		openMobileNavbar();
+	}
+});
+
+const navbarMenu = navbar.querySelector(".navbar-menu");
+const navbarLinksContainer = navbar.querySelector(".navbar-links");
+
+navbarLinksContainer.addEventListener("click", (clickEvent) => {
+	clickEvent.stopPropagation();
+});
+
+navbarMenu.addEventListener("click", closeMobileNavbar);
