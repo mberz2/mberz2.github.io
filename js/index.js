656cd6cc0e92e7736edb61b402f9c4a619f4b358
Matt Berzinskas <mberz2@pdx.edu>
2021-08-18 14:36:26 -0700

    Added Scripting Support for Multiple Modals

--- js/index.js
+++ js/index.js
@@ -41,30 +41,76 @@ navbarLinks.forEach((e) => {
 
 
 
+        // Get the modal
 
-	
-// Get the modal
-var modal = document.getElementById("myModal");
+        var modalparent = document.getElementsByClassName("modal_multi");
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
+        // Get the button that opens the modal
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+        var modal_btn_multi = document.getElementsByClassName("myBtn_multi");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
+        // Get the <span> element that closes the modal
+        var span_close_multi = document.getElementsByClassName("close_multi");
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
+        // When the user clicks the button, open the modal
+        function setDataIndex() {
 
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+            for (i = 0; i < modal_btn_multi.length; i++)
+            {
+                modal_btn_multi[i].setAttribute('data-index', i);
+                modalparent[i].setAttribute('data-index', i);
+                span_close_multi[i].setAttribute('data-index', i);
+            }
+        }
+
+
+
+        for (i = 0; i < modal_btn_multi.length; i++)
+        {
+            modal_btn_multi[i].onclick = function() {
+                var ElementIndex = this.getAttribute('data-index');
+                modalparent[ElementIndex].style.display = "block";
+            };
+
+            // When the user clicks on <span> (x), close the modal
+            span_close_multi[i].onclick = function() {
+                var ElementIndex = this.getAttribute('data-index');
+                modalparent[ElementIndex].style.display = "none";
+            };
+
+        }
+
+        window.onload = function() {
+
+            setDataIndex();
+        };
+
+        window.onclick = function(event) {
+            if (event.target === modalparent[event.target.getAttribute('data-index')]) {
+                modalparent[event.target.getAttribute('data-index')].style.display = "none";
+            }
+        };
+
+//XXXXXXXXXXXXXXXXXXXXXXX    Modified old code    XXXXXXXXXXXXXXXXXXXXXXXXXX
+
+// // Get the modal
+
+//         var modal = document.getElementById('myModal');
+
+// // Get the button that opens the modal
+//         var btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+//         var span = modal.getElementsByClassName("close")[0]; // Modified by dsones uk
+
+// // When the user clicks on the button, open the modal
+
+//         btn.onclick = function() {
+
+//             modal.style.display = "block";
+//         }
+
+// // When the user clicks on <span> (x), close the modal
+//         span.onclick = function() {
+//             modal.style.display = "none";
+//         }
